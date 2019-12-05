50e9b1061c9075c2525d12a9da407de4aafc67bc
Nadya <52658793+shanshay@users.noreply.github.com>
2019-12-05 14:12:20 +0300

    Update test_loto.py

--- test_loto.py
+++ test_loto.py
@@ -2,6 +2,7 @@ import pytest
 from class_loto import Gamer
 from class_loto import Leading
 
+
 class TestGamer:
 
     def test_create_card(self):
@@ -14,9 +15,14 @@ class TestGamer:
         assert gamer.count == 0
         assert gamer.test == False
 
+    def test_check_count(self):
+        gamer = Gamer(name='Nadya', count=15, test=False)
+        gamer.check_count() == (f'Игра окончена! {gamer.name} выиграл! У {gamer.name} все цифры зачеркнуты!')
+
 class TestLeading:
 
     def test_init(self):
         leading = Leading()
         leading.step = 15
         assert leading.step == 15
+        assert len(leading.barrel) == 90
